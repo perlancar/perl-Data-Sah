abd9046830e8f06c2f4263a81eb8c07ec3cc3128
Steven Haryanto (on Asus EEEPC) <stevenharyanto@gmail.com>
2010-09-27 09:16:12 +0700

    postpone types 'datetime' and 'duration' to spec v1.1

--- lib/Data/Schema/Spec/v10/Type/DateTimeSet.pm
+++
@@ -1,4 +0,0 @@
-package Data::Schema::Spec::v10::Type::DateTimeSet;
-# ABSTRACT: Specificationn for date/time set
-
-1;
--- lib/Data/Schema/Spec/v10/Type/Duration.pm
+++
@@ -1,4 +0,0 @@
-package Data::Schema::Spec::v10::Type::Duration;
-# ABSTRACT: Specificationn for date/time duration
-
-1;
