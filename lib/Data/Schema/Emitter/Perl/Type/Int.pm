640a4cc5613ed0cd3504291f4d5e4d9120f1531f
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2010-10-22 08:18:29 +0700

    order attr handlers

--- lib/Data/Schema/Emitter/Perl/Type/Int.pm
+++ lib/Data/Schema/Emitter/Perl/Type/Int.pm
@@ -5,14 +5,13 @@ use Any::Moose;
 extends 'Data::Schema::Emitter::Perl::Type::Num';
 with 'Data::Schema::Spec::v10::Type::Int';
 
-sub attr_mod {
+after attr_SANITY => sub {
     my ($self, %args) = @_;
     my $attr = $args{attr};
     my $e = $self->emitter;
 
-    $e->errif($attr,
-              '$data % '.$attr->{value}.'->[0] != '.$attr->{value}.'->[1]');
-}
+    $e->errif($attr, 'int($data) != $data', 'last ATTRS');
+};
 
 sub attr_divisible_by {
     my ($self, %args) = @_;
@@ -22,6 +21,15 @@ sub attr_divisible_by {
     $e->errif($attr, '$data % '.$attr->{value}.' != 0');
 }
 
+sub attr_mod {
+    my ($self, %args) = @_;
+    my $attr = $args{attr};
+    my $e = $self->emitter;
+
+    $e->errif($attr,
+              '$data % '.$attr->{value}.'->[0] != '.$attr->{value}.'->[1]');
+}
+
 sub attr_not_divisible_by {
     my ($self, %args) = @_;
     my $attr = $args{attr};
@@ -29,14 +37,6 @@ sub attr_not_divisible_by {
 
     $e->errif($attr, '$data % '.$attr->{value}.' == 0');
 }
-
-after attr_SANITY => sub {
-    my ($self, %args) = @_;
-    my $attr = $args{attr};
-    my $e = $self->emitter;
-
-    $e->errif($attr, 'int($data) != $data', 'last ATTRS');
-};
 
 __PACKAGE__->meta->make_immutable;
 no Any::Moose;
