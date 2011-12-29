d16abdcf810bcc83787a5280273dedd528dc930c
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-12-29 08:44:33 +0700

    Add some common schemas

--- lib/Data/Sah/Schemas/Common.pm
+++ lib/Data/Sah/Schemas/Common.pm
@@ -13,8 +13,31 @@ sub schemas {
             isa_regex => 1,
         }],
 
-         => [
-        ],
+        pos_int => [int => {
+            name      => 'pos_int',
+            summary   => 'Positive integer',
+            min       => 0,
+        }],
+
+        neg_int => [int => {
+            name      => 'neg_int',
+            summary   => 'Positive integer',
+            max       => 0,
+        }],
+
+        nat_num => [int => {
+            name        => 'nat_num',
+            summary     => 'Natural number',
+            description => <<_,
+
+Natural numbers are whole numbers starting from 1, used for counting ('there are
+6 coins on the table') and ordering ('this is the 3rd largest city in the
+country').
+
+_
+            min         => 1,
+        }],
+
     };
 }
 
