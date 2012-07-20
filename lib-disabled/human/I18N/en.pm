633415c0c07df33ea0762b147985fcbb7578fc22
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-20 22:33:04 +0700

    Disable stuffs that aren't ready for release (2)

---
+++ lib-disabled/human/I18N/en.pm
@@ -0,0 +1,16 @@
+package Data::Sah::Compiler::human::I18N::en;
+use parent qw(Data::Sah::Compiler::perl::I18N);
+
+use Locale::Maketext::Lexicon::Gettext;
+our %Lexicon = %{ Locale::Maketext::Lexicon::Gettext->parse(<DATA>) };
+
+# VERSION
+
+#use Data::Dump; dd \%Lexicon;
+
+1;
+# ABSTRACT: English translation
+__DATA__
+msgid  ""
+msgstr ""
+
---
+++ lib-disabled/js/TH.pm
@@ -0,0 +1,9 @@
+package Data::Sah::Compiler::perl::TH;
+
+use Moo;
+extends 'Data::Sah::Compiler::BaseProg::TH';
+
+1;
+# ABSTRACT: Base class for perl type handlers
+
+=cut
