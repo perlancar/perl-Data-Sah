a01156a54fdf095162de7203487e2d96faa9532f
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2012-10-26 18:32:54 +0700

    perl: Implement bool

---
+++ lib/Data/Sah/Compiler/perl/TH/bool.pm
@@ -0,0 +1,83 @@
+package Data::Sah::Compiler::perl::TH::bool;
+
+use 5.010;
+use Log::Any '$log';
+use Moo;
+extends 'Data::Sah::Compiler::perl::TH';
+with 'Data::Sah::Type::bool';
+
+# VERSION
+
+sub handle_type_check {
+    my ($self, $cd) = @_;
+    my $c = $self->compiler;
+
+    my $dt = $cd->{data_term};
+    $cd->{_ccl_check_type} = "!ref($dt)";
+}
+
+sub superclause_comparable {
+    my ($self, $which, $cd) = @_;
+    my $c = $self->compiler;
+
+    $c->handle_clause(
+        $cd,
+        on_term => sub {
+            my ($self, $cd) = @_;
+            my $ct = $cd->{cl_term};
+            my $dt = $cd->{data_term};
+
+            if ($which eq 'is') {
+                $c->add_ccl($cd, "($dt ? 1:0) == ($ct ? 1:0)");
+            } elsif ($which eq 'in') {
+                $c->add_ccl($cd, "($dt ? 1:0) ~~ [map {$_?1:0} \@{$ct}]");
+            }
+        },
+    );
+}
+
+sub superclause_sortable {
+    my ($self, $which, $cd) = @_;
+    my $c = $self->compiler;
+
+    $c->handle_clause(
+        $cd,
+        on_term => sub {
+            my ($self, $cd) = @_;
+            my $cv = $cd->{cl_value};
+            my $ct = $cd->{cl_term};
+            my $dt = $cd->{data_term};
+
+            if ($which eq 'min') {
+                $c->add_ccl($cd, "($dt ? 1:0) >= ($ct ? 1:0)");
+            } elsif ($which eq 'xmin') {
+                $c->add_ccl($cd, "($dt ? 1:0) > ($ct ? 1:0)");
+            } elsif ($which eq 'max') {
+                $c->add_ccl($cd, "($dt ? 1:0) <= ($ct ? 1:0)");
+            } elsif ($which eq 'xmax') {
+                $c->add_ccl($cd, "($dt ? 1:0) < ($ct ? 1:0)");
+            } elsif ($which eq 'between') {
+                if ($cd->{cl_is_expr}) {
+                    $c->add_ccl($cd, "($dt ? 1:0) >= ($ct\->[0] ? 1:0) && ".
+                                    "($dt ? 1:0) <= ($ct\->[1] ? 1:0)");
+                } else {
+                    # simplify code
+                    $c->add_ccl($cd, "($dt ? 1:0) >= ($cv->[0] ? 1:0) && ".
+                                    "($dt ? 1:0) <= ($cv->[1] ? 1:0)");
+                }
+            } elsif ($which eq 'xbetween') {
+                if ($cd->{cl_is_expr}) {
+                    $c->add_ccl($cd, "($dt ? 1:0) > ($ct\->[0] ? 1:0) && ".
+                                    "($dt ? 1:0) < ($ct\->[1] ? 1:0)");
+                } else {
+                    # simplify code
+                    $c->add_ccl($cd, "($dt ? 1:0) > ($cv->[0] ? 1:0) && ".
+                                    "($dt ? 1:0) < ($cv->[1] ? 1:0)");
+                }
+            }
+        },
+    );
+}
+
+1;
+# ABSTRACT: perl's type handler for type "bool"
---
+++ lib/Data/Sah/Type/bool.pm
@@ -0,0 +1,13 @@
+package Data::Sah::Type::bool;
+
+use Moo::Role;
+with 'Data::Sah::Type::BaseType';
+with 'Data::Sah::Type::Comparable';
+with 'Data::Sah::Type::Sortable';
+
+# VERSION
+
+1;
+# ABSTRACT: bool type
+
+=cut
