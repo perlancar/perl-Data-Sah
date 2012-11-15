b7a6ff58388cd1680e90742cf5cd0eda7257cbbd
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2012-11-15 10:20:11 +0700

    Delete unneeded files

--- lib/Data/Sah/Compiler/human/I18N.pm
+++
@@ -1,9 +0,0 @@
-package Data::Sah::Compiler::human::I18N;
-
-use base 'Locale::Maketext';
-use Locale::Maketext::Lexicon;
-
-# VERSION
-
-1;
-# ABSTRACT: Project class for Data::Sah::Compiler::human
