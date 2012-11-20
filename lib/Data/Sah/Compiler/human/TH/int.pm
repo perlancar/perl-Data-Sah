a5cd95ce08d7585ff021a08d822a50ff338c4c66
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2012-11-20 18:29:15 +0700

    Change ccl key name

--- lib/Data/Sah/Compiler/human/TH/int.pm
+++ lib/Data/Sah/Compiler/human/TH/int.pm
@@ -13,7 +13,7 @@ sub handle_type {
     my $c = $self->compiler;
 
     $c->add_ccl($cd, {
-        human => ["integer", "integers"],
+        text  => ["integer", "integers"],
         type  => 'noun',
     });
 }
