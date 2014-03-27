431d136aa8e3198d8cc8e8d6a9ca87609a1027e2
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-03-27 15:25:38 +0700

    Refactor: extract method normalize_clset, normalize value clset clause (note: incomplete because tests break)

--- lib/Data/Sah.pm
+++ lib/Data/Sah.pm
@@ -56,6 +56,115 @@ our $funcset_re     = qr/\A(?:[A-Za-z_]\w*::)*[A-Za-z_]\w*\z/;
 our $compiler_re    = qr/\A[A-Za-z_]\w*\z/;
 our $clause_attr_on_empty_clause_re = qr/\A(?:\.[A-Za-z_]\w*)+\z/;
 
+# return a shallow copy of the original hash.
+sub normalize_clset {
+    my $self;
+    if (Scalar::Util::blessed($_[0])) {
+        $self = shift;
+    } else {
+        $self = __PACKAGE__->new;
+    }
+
+    my $clset0 = shift;
+    my $opts   = shift // {};
+
+    my $clset = {};
+    for my $c (sort keys %$clset0) {
+        my $c0 = $c;
+
+        my $v = $clset0->{$c};
+
+        # ignore expression
+        my $expr;
+        if ($c =~ s/=\z//) {
+            $expr++;
+            # XXX currently can't disregard merge prefix when checking
+            # conflict
+            die "Conflict between '$c=' and '$c'" if exists $clset0->{$c};
+            $clset->{"$c.is_expr"} = 1;
+            }
+
+        my $sc = "";
+        my $cn;
+        {
+            my $errp = "Invalid clause name syntax '$c0'"; # error prefix
+            if (!$expr && $c =~ s/\A!(?=.)//) {
+                die "$errp, syntax should be !CLAUSE"
+                    unless $c =~ $clause_name_re;
+                $sc = "!";
+            } elsif (!$expr && $c =~ s/(?<=.)\|\z//) {
+                die "$errp, syntax should be CLAUSE|"
+                    unless $c =~ $clause_name_re;
+                $sc = "|";
+            } elsif (!$expr && $c =~ s/(?<=.)\&\z//) {
+                die "$errp, syntax should be CLAUSE&"
+                    unless $c =~ $clause_name_re;
+                $sc = "&";
+            } elsif (!$expr && $c =~ /\A([^.]+)(?:\.(.+))?\((\w+)\)\z/) {
+                my ($c2, $a, $lang) = ($1, $2, $3);
+                die "$errp, syntax should be CLAUSE(LANG) or C.ATTR(LANG)"
+                    unless $c2 =~ $clause_name_re &&
+                        (!defined($a) || $a =~ $attr_re);
+                $sc = "(LANG)";
+                $cn = $c2 . (defined($a) ? ".$a" : "") . ".alt.lang.$lang";
+            } elsif ($c !~ $clause_re &&
+                         $c !~ $clause_attr_on_empty_clause_re) {
+                die "$errp, please use letter/digit/underscore only";
+            }
+        }
+
+        # XXX can't disregard merge prefix when checking conflict
+        if ($sc eq '!') {
+            die "Conflict between clause shortcuts '!$c' and '$c'"
+                if exists $clset0->{$c};
+            die "Conflict between clause shortcuts '!$c' and '$c|'"
+                if exists $clset0->{"$c|"};
+            die "Conflict between clause shortcuts '!$c' and '$c&'"
+                if exists $clset0->{"$c&"};
+            $clset->{$c} = $v;
+            $clset->{"$c.op"} = "not";
+        } elsif ($sc eq '&') {
+            die "Conflict between clause shortcuts '$c&' and '$c'"
+                if exists $clset0->{$c};
+            die "Conflict between clause shortcuts '$c&' and '$c|'"
+                if exists $clset0->{"$c|"};
+            die "Clause 'c&' value must be an array"
+                unless ref($v) eq 'ARRAY';
+            $clset->{$c} = $v;
+            $clset->{"$c.op"} = "and";
+        } elsif ($sc eq '|') {
+            die "Conflict between clause shortcuts '$c|' and '$c'"
+                if exists $clset0->{$c};
+            die "Clause 'c|' value must be an array"
+                unless ref($v) eq 'ARRAY';
+            $clset->{$c} = $v;
+            $clset->{"$c.op"} = "or";
+        } elsif ($sc eq '(LANG)') {
+            die "Conflict between clause '$c' and '$cn'"
+                if exists $clset0->{$cn};
+            $clset->{$cn} = $v;
+        } else {
+            $clset->{$c} = $v;
+        }
+
+    }
+    $clset->{req} = 1 if $opts->{has_req};
+
+    # the 'clset' clause contains clause set, so we recursively normalize it
+    if ($clset->{clset}) {
+        local $opts->{has_req};
+        if ($clset->{'clset.op'} && $clset->{'clset.op'} =~ /and|or/) {
+            # multiple clause sets
+            $clset->{clset} = map { $self->normalize_clset($_, $opts) }
+                @{ $clset->{clset} };
+        } else {
+            $clset->{clset} = $self->normalize_clset($_, $opts);
+        }
+    }
+
+    $clset;
+}
+
 # produce a 2-level copy of schema, so it's safe to add/delete/modify the
 # normalized schema's clause set and extras (but clause set's and extras' values
 # are still references to the original).
@@ -114,88 +223,7 @@ sub normalize_schema {
         }
 
         # check clauses and parse shortcuts (!c, c&, c|, c=)
-        my $clset = {};
-        for my $c (sort keys %$clset0) {
-            my $c0 = $c;
-
-            my $v = $clset0->{$c};
-
-            # ignore expression
-            my $expr;
-            if ($c =~ s/=\z//) {
-                $expr++;
-                # XXX currently can't disregard merge prefix when checking
-                # conflict
-                die "Conflict between '$c=' and '$c'" if exists $clset0->{$c};
-                $clset->{"$c.is_expr"} = 1;
-            }
-
-            my $sc = "";
-            my $cn;
-            {
-                my $errp = "Invalid clause name syntax '$c0'"; # error prefix
-                if (!$expr && $c =~ s/\A!(?=.)//) {
-                    die "$errp, syntax should be !CLAUSE"
-                        unless $c =~ $clause_name_re;
-                    $sc = "!";
-                } elsif (!$expr && $c =~ s/(?<=.)\|\z//) {
-                    die "$errp, syntax should be CLAUSE|"
-                        unless $c =~ $clause_name_re;
-                    $sc = "|";
-                } elsif (!$expr && $c =~ s/(?<=.)\&\z//) {
-                    die "$errp, syntax should be CLAUSE&"
-                        unless $c =~ $clause_name_re;
-                    $sc = "&";
-                } elsif (!$expr && $c =~ /\A([^.]+)(?:\.(.+))?\((\w+)\)\z/) {
-                    my ($c2, $a, $lang) = ($1, $2, $3);
-                    die "$errp, syntax should be CLAUSE(LANG) or C.ATTR(LANG)"
-                        unless $c2 =~ $clause_name_re &&
-                            (!defined($a) || $a =~ $attr_re);
-                    $sc = "(LANG)";
-                    $cn = $c2 . (defined($a) ? ".$a" : "") . ".alt.lang.$lang";
-                } elsif ($c !~ $clause_re &&
-                             $c !~ $clause_attr_on_empty_clause_re) {
-                    die "$errp, please use letter/digit/underscore only";
-                }
-            }
-
-            # XXX can't disregard merge prefix when checking conflict
-            if ($sc eq '!') {
-                die "Conflict between clause shortcuts '!$c' and '$c'"
-                    if exists $clset0->{$c};
-                die "Conflict between clause shortcuts '!$c' and '$c|'"
-                    if exists $clset0->{"$c|"};
-                die "Conflict between clause shortcuts '!$c' and '$c&'"
-                    if exists $clset0->{"$c&"};
-                $clset->{$c} = $v;
-                $clset->{"$c.op"} = "not";
-            } elsif ($sc eq '&') {
-                die "Conflict between clause shortcuts '$c&' and '$c'"
-                    if exists $clset0->{$c};
-                die "Conflict between clause shortcuts '$c&' and '$c|'"
-                    if exists $clset0->{"$c|"};
-                die "Clause 'c&' value must be an array"
-                    unless ref($v) eq 'ARRAY';
-                $clset->{$c} = $v;
-                $clset->{"$c.op"} = "and";
-            } elsif ($sc eq '|') {
-                die "Conflict between clause shortcuts '$c|' and '$c'"
-                    if exists $clset0->{$c};
-                die "Clause 'c|' value must be an array"
-                    unless ref($v) eq 'ARRAY';
-                $clset->{$c} = $v;
-                $clset->{"$c.op"} = "or";
-            } elsif ($sc eq '(LANG)') {
-                die "Conflict between clause '$c' and '$cn'"
-                    if exists $clset0->{$cn};
-                $clset->{$cn} = $v;
-            } else {
-                $clset->{$c} = $v;
-            }
-
-        }
-        $clset->{req} = 1 if $has_req;
-
+        my $clset = $self->normalize_clset($clset0, {has_req=>$has_req});
         if (defined $extras) {
             die "For array form with 3 elements, extras must be hash"
                 unless ref($extras) eq 'HASH';
@@ -487,6 +515,14 @@ Example:
 Normalize a schema, e.g. change C<int*> into C<< [int => {req=>1}] >>, as well
 as do some sanity checks on it. Returns the normalized schema if succeeds, or
 dies on error.
+
+Can also be used as a function.
+
+=head2 $sah->normalize_clset($clset[, \%opts]) => HASH
+
+Normalize a clause set, e.g. change C<< {"!match"=>"abc"} >> into C<<
+{"match"=>"abc", "match.op"=>"not"} >>. Produce a shallow copy of the input
+clause set hash.
 
 Can also be used as a function.
 
