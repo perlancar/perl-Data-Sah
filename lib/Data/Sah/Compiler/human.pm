a4a90615da5b4b78bdc1a7707915c62f222d800e
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-24 14:15:03 +0700

    [opt] Replace producing literal from using JSON to using Data::Dmp to shave off more startup overhead (5-7ms)

--- lib/Data/Sah/Compiler/human.pm
+++ lib/Data/Sah/Compiler/human.pm
@@ -8,6 +8,7 @@ use strict;
 use warnings;
 #use Log::Any qw($log);
 
+use Data::Dmp qw(dmp);
 use Mo qw(build default);
 use POSIX qw(locale_h);
 use Text::sprintfn;
@@ -67,24 +68,7 @@ sub literal {
     my ($self, $val) = @_;
 
     return $val unless ref($val);
-
-    # for now we use JSON. btw, JSON does obey locale setting, e.g. [1.2]
-    # encoded to "[1,2]" in id_ID.
-    state $json = do {
-        require JSON;
-        JSON->new->allow_nonref;
-    };
-
-    # we also need cleaning if we use json, since json can't handle qr//, for
-    # one.
-    state $cleanser = do {
-        require Data::Clean::JSON;
-        Data::Clean::JSON->get_cleanser;
-    };
-
-    # XXX for nicer output, perhaps say "empty array" instead of "[]", "empty
-    # hash" instead of "{}", etc.
-    $json->encode($cleanser->clone_and_clean($val));
+    dmp($val);
 }
 
 # translate
